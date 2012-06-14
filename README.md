ddd232564467fe2624af80e0ac80998227dac263
Xavier Noria <fxn@hashref.com>
2012-06-15 00:50:04 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+This is a fairly simple Sudoku solver written in C. And exercise in bactracking I did back in 2005 to cheat in a contest of a newspaper. They published a daily Sudoku, and you had to send the solution to win something I have forgotten. Come on, who wants to solve a Sudoku when you can solve them all, uh?
+
+The backtracking loop is compact, it is kind of generic to many backtracking problems I believe.
+
+Cells are integers. Rows, columns, and the nine sub-squares maintain some bit masks that allows the program to check stuff using bitwise operators. That proved to be a fast approach compared to other options.
+
+Other than that, the program is not very smart, just brute force backtracking with the obvious optimizations.
+
+To compile _sudoku.c_ run
+
+    gcc -O3 -std=c99 -o sudoku sudoku.c
+
+The program receives the square as a series of numbers of three digits. Each argument represents one of the known numbers in the Sudoku and is entered as _row-col-number_, where _row_ and _col_ are 1-based.
+
+This is for example the solution to the [Sudoku in the Wikipedia](http://en.wikipedia.org/wiki/Sudoku):
+
+    $ time ./sudoku 115 123 157 216 241 259 265 329 338 386 418 456 493 514 548 563 591 617 652 696 726 772 788 844 851 869 895 958 987 999
+    +---------+---------+---------+
+    | 5  3  4 | 6  7  8 | 9  1  2 |
+    | 6  7  2 | 1  9  5 | 3  4  8 |
+    | 1  9  8 | 3  4  2 | 5  6  7 |
+    +---------+---------+---------+
+    | 8  5  9 | 7  6  1 | 4  2  3 |
+    | 4  2  6 | 8  5  3 | 7  9  1 |
+    | 7  1  3 | 9  2  4 | 8  5  6 |
+    +---------+---------+---------+
+    | 9  6  1 | 5  3  7 | 2  8  4 |
+    | 2  8  7 | 4  1  9 | 6  3  5 |
+    | 3  4  5 | 2  8  6 | 1  7  9 |
+    +---------+---------+---------+
+
+    real	0m0.007s
+    user	0m0.001s
+    sys		0m0.003s
+
+Since this was an exercise control error of the arguments is totally primitive. Basically, you either enter the right stuff or else get a segmentation fault.
---
+++ sudoku.c
@@ -0,0 +1,200 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+void set_cell(int i, int j, int n);
+int clear_cell(int i, int j);
+void init_known(size_t count, char** cells);
+bool is_available(int i, int j, int n);
+bool advance_cell(int i, int j);
+void solve_sudoku(void);
+void init_globals(void);
+void* xcalloc(size_t count, size_t size);
+void print_matrix(void);
+void print_separator(void);
+
+/* The Sudoku matrix itself. */
+int** matrix = NULL;
+
+/* Which numbers were given as known in the problem. */
+int** known = NULL;
+
+/* An array of nine integers, each of one representing a sub-square.
+Each integer has its nth-bit on iff n belongs to the corresponding sub-square. */
+int* squares = NULL;
+
+/* An array of nine integers, each of one representing a row.
+Each integer has its nth-bit on iff n belongs to the corresponding row. */
+int* rows = NULL;
+
+/* An array of nine integers, each of one representing a column.
+Each integer has its nth-bit on iff n belongs to the corresponding column. */
+int* cols = NULL;
+
+/* An array with some powers of 2 to avoid shifting all the time. */
+int* bits = NULL;
+
+
+int main(int argc, char** argv)
+{
+    init_globals();
+    init_known(argc-1, argv+1);
+
+    solve_sudoku();
+    print_matrix();
+
+    return EXIT_SUCCESS;
+}
+
+
+/* Stores the number n in the cell (i, j), and turns on the corresponding
+bits in rows, cols, and squares. */
+void set_cell(int i, int j, int n)
+{
+    matrix[i][j] = n;
+    rows[i] |= bits[n];
+    cols[j] |= bits[n];
+    squares[(i/3)*3 + j/3] |= bits[n];
+}
+
+/* Clears the cell (i, j) and turns off the corresponding bits in rows, cols,
+and squares. Returns the number it contained. */
+int clear_cell(int i, int j)
+{
+    int n = matrix[i][j];
+    matrix[i][j] = 0;
+    rows[i] &= ~bits[n];
+    cols[j] &= ~bits[n];
+    squares[(i/3)*3 + j/3] &= ~bits[n];
+    return n;
+}
+
+/* Processes the program arguments. Each argument is assumed to be a string
+with three digits row-col-number, 1-based, representing the known cells in the
+Sudoku. For example, "123" means there is a 3 in the cell (0, 1). */
+void init_known(size_t count, char** cells)
+{
+    for (int c = 0; c < count; ++c) {
+        char* cell = cells[c];
+        int i, j, n;
+        if (sscanf(cell, "%1d%1d%1d", &i, &j, &n)) {
+            set_cell(i-1, j-1, n);
+            known[i-1][j-1] = 1;
+        } else {
+            printf("bad input token: %s\n", cell);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+/* Can we put n in the cell (i, j)? */
+bool is_available(int i, int j, int n)
+{
+    return (rows[i] & bits[n]) == 0 &&
+           (cols[j] & bits[n]) == 0 &&
+           (squares[(i/3)*3 + j/3] & bits[n]) == 0;
+}
+
+/* Tries to fill the cell (i, j) with the next available number.
+Returns a flag to indicate if it succeeded. */
+bool advance_cell(int i, int j)
+{
+    int n = clear_cell(i, j);
+    while (++n <= 9) {
+        if (is_available(i, j, n)) {
+            set_cell(i, j, n);
+            return true;
+        }
+    }
+    return false;
+}
+
+/* The main function, a fairly generic backtracking algorithm. */
+void solve_sudoku(void)
+{
+    int pos = 0;
+    while (1) {
+        while (pos < 81 && known[pos/9][pos%9]) {
+            ++pos;
+        }
+        if (pos >= 81) {
+            break;
+        }
+        if (advance_cell(pos/9, pos%9)) {
+            ++pos;
+        } else {
+            do {
+                --pos;
+            } while (pos >= 0 && known[pos/9][pos%9]);
+            if (pos < 0) {
+                break;
+            }
+        }
+    }
+}
+
+/* Allocates memory and stuff. */
+void init_globals(void)
+{
+    matrix = xcalloc(9, sizeof(int*));
+    known  = xcalloc(9, sizeof(int*));
+    for (int i = 0; i < 9; i++) {
+        matrix[i] = xcalloc(9, sizeof(int));
+        known[i]  = xcalloc(9, sizeof(int));
+    }
+
+    squares = xcalloc(9, sizeof(int));
+    rows = xcalloc(9, sizeof(int));
+    cols = xcalloc(9, sizeof(int));
+
+    bits = xcalloc(10, sizeof(int));
+    for (int n = 1; n < 10; n++) {
+        bits[n] = 1 << n;
+    }
+}
+
+void* xcalloc(size_t count, size_t size)
+{
+    void* p;
+    p = calloc(count, size);
+    if (p == NULL) {
+        perror("Memory allocation failure");
+        exit(EXIT_FAILURE);
+    }
+    return p;
+}
+
+/* Prints the matrix using some ANSI escape sequences
+to distinguish the originally known numbers. */
+void print_matrix(void)
+{
+    for (int i = 0; i < 9; ++i) {
+        if ((i % 3) == 0) {
+            print_separator();
+        }
+        for (int j = 0; j < 9; j++) {
+            int cell = matrix[i][j];
+            if ((j % 3) == 0) {
+                printf("\e[1;34m|\e[0m ");
+            } else {
+                printf(" ");
+            }
+            if (known[i][j]) {
+                printf("\e[1;34m%d\e[0m ", cell);
+            } else {
+                printf("%d ", cell);
+            }
+        }
+        printf("|\n");
+    }
+    print_separator();
+}
+
+/* Utility to print lines and crosses, used by print_matrix. */
+void print_separator(void)
+{
+    for (int i = 0; i < 3; ++i) {
+        printf("\e[1;34m+---------\e[0m");
+    }
+    printf("\e[1;34m+\n\e[0m");
+}
